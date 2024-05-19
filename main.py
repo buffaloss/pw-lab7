03603413b063bebc49a1d50165e362d5551ee591
buffaloss <beatricia.golban@isa.utm.md>
2024-05-19 18:21:54 +0300

    Create get locations endpoint

--- main.py
+++ main.py
@@ -9,11 +9,32 @@ def location(id):
     try:
         with open('locations.json', 'r') as file:
             locations = loads(file.read())
-        
         if id in locations:
             return jsonify(locations[id]), 200
         else:
             return jsonify({'msg': 'Not found'}), 404
+
+    except:
+        return jsonify({'msg': 'Internal server error'}), 500
+
+
+@app.route('/api/location', methods=['GET'])
+def locations():
+    try:
+        with open('locations.json', 'r') as file:
+            locations = loads(file.read())
+        
+        if request.args.get('from'):
+            from_id = int(request.args.get('from'))
+            if request.args.get('count'):
+                count = int(request.args.get('count'))
+                for i in range(count):
+                    if str(from_id + i) not in locations:
+                        return jsonify({'msg': 'Not found'}), 404
+                response = {}
+                for i in range(count):
+                    response[str(from_id + i)] = locations[str(from_id + i)]
+                return jsonify(response), 200
 
     except:
         return jsonify({'msg': 'Internal server error'}), 500
